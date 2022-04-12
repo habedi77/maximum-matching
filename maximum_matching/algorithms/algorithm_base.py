f207e51851160b769b01c973b7085b53c5c7df55
habedi77 <hossein.abedi1998@gmail.com>
2022-04-11 22:15:50 -0500

    add AlgorithmBase

---
+++ maximum_matching/algorithms/algorithm_base.py
@@ -0,0 +1,25 @@
+from ..graphs.bipartite_graph import BaseBipartiteGraph
+from typing import Union, Tuple, Any
+
+from abc import ABC, abstractmethod
+
+
+class AlgorithmBase(ABC):
+    """
+    Base class for matching algorithms
+
+    Algorithm implementations override the run class
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    @abstractmethod
+    def run(self, graph: BaseBipartiteGraph) -> Any:
+        """
+        Run the matching algorithm on the given graph
+
+        :param graph: a graph instance from the BaseBipartiteGraph abstract
+        :return: TODO
+        """
+        pass
