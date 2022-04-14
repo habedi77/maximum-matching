ef437d3d7dd6b8dc0c1db1086d39fe92911fe2b1
Jamilush <work.jamilsiam@gmail.com>
2022-04-13 19:51:06 -0500

    Removed print statements

--- maximum_matching/algorithms/oblivious.py
+++ maximum_matching/algorithms/oblivious.py
@@ -8,18 +8,15 @@ from maximum_matching.graphs.graph_base import BipartiteSet, GraphBase
 class Oblivious(AlgorithmBase): 
     
     def run(self, graph: GraphBase) -> Any:
-        # print("Run Oblivious")
         known_nodes = graph.b_get_independent_set(BipartiteSet.left)
         unknown_nodes = graph.b_get_independent_set(BipartiteSet.right)
 
         matched = {}
         for known_node in known_nodes:
-            print("known_node :", known_node)
             matched[known_node] = False
 
         count_matches = 0
         for unknown_node in unknown_nodes:
-            # print("unknown_node :", unknown_node)
             neighbours = graph.list(unknown_node)
 
             if len(neighbours) > 0:
@@ -29,8 +26,5 @@ class Oblivious(AlgorithmBase):
                 if matched.get(random_neighbour) is False:
                     matched[random_neighbour] = True
                     count_matches += 1
-                elif matched.get(random_neighbour) is None:
-                    print("unknown_node :", unknown_node)
-                    print("Where is neighbour? :", random_neighbour)
         
-        print("Matched: ", count_matches)
+        return count_matches
