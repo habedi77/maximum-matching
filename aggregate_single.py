9c69475919458e98cb5b0db366434a62be613fc3
habedi77 <hossein.abedi1998@gmail.com>
2022-05-05 21:20:31 -0500

    testing feldman

--- aggregate_single.py
+++ aggregate_single.py
@@ -17,12 +17,12 @@ PRINT_OUTPUT = False
 
 # List the algorithms you would like the program to run
 _algorithms = [
-    Vaz(),  # Vazirani (Online)
-    Rand(),
-    MinDeg(),
-    Oblivious(),
-    # FeldmanTSM(),
-    MaxFlow()
+    # Vaz(),  # Vazirani (Online)
+    # Rand(),
+    # MinDeg(),
+    # Oblivious(),
+    FeldmanTSM(),
+    # MaxFlow()
 ]
 
 
